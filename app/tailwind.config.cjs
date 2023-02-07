b96e85a41b9cf22b52be7d0ca191c11e6a3ffd3b
cemgthedev <cemg.the.dev@gmail.com>
2023-02-07 13:04:02 -0300

    corrigindo menu dropdown com as categorias da API Fake Store

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -6,7 +6,8 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        'menu': '0 0 10px 2px #06b6d4'
+        'menu': '0 0 10px 2px #06b6d4',
+        'card': '0 0 10px 2px #4b5563'
       }
     },
   },
